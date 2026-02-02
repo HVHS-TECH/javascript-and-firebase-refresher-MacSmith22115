afc582bea24a164470f4f32dfb81dd194eda8e21
MacSmith22115 <22115ms@hvhs.school.nz>
2026-02-03 12:21:27 +1300

    added Firebase initialization

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var messageSpace = getElementbyId("welcomeMessage");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAOPnMr_-61TVbtvgW3VWvh6af_KOPYwk",
+    authDomain: "comp-firebase-revision.firebaseapp.com",
+    projectId: "comp-firebase-revision",
+    storageBucket: "comp-firebase-revision.firebasestorage.app",
+    messagingSenderId: "1028621260717",
+    appId: "1:1028621260717:web:28f92cc83af73f2655f324"
+};
+
+const app = initializeApp(firebaseConfig);
+    
+var messageSpace = document.getElementById("welcomeMessage");
 messageSpace.innerHTML = "You've connected to the JavaScript!";
+
+
