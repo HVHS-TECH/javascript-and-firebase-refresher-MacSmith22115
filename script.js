dce507adec6581185d80e8d2c3e643499c28926c
MacSmith22115 <22115ms@hvhs.school.nz>
2026-02-03 18:07:54 +1300

    HTML Stuff & Firebase Write

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+    import {getDatabase, set, ref, get, off,onValue,update, query,remove, orderByValue} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCAOPnMr_-61TVbtvgW3VWvh6af_KOPYwk",
-    authDomain: "comp-firebase-revision.firebaseapp.com",
-    projectId: "comp-firebase-revision",
-    storageBucket: "comp-firebase-revision.firebasestorage.app",
-    messagingSenderId: "1028621260717",
-    appId: "1:1028621260717:web:28f92cc83af73f2655f324"
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyCAOPnMr_-61TVbtvgW3VWvh6af_KOPYwk",
+        authDomain: "comp-firebase-revision.firebaseapp.com",
+        projectId: "comp-firebase-revision",
+        storageBucket: "comp-firebase-revision.firebasestorage.app",
+        messagingSenderId: "1028621260717",
+        appId: "1:1028621260717:web:28f92cc83af73f2655f324",
+        databaseURL: "https://comp-firebase-revision-default-rtdb.firebaseio.com/"
+    };
 
-const app = initializeApp(firebaseConfig);
-    
-var messageSpace = document.getElementById("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+    const app = initializeApp(firebaseConfig);
+    var messageSpace = document.getElementById("welcomeMessage");
+    var input = document.getElementById("input");
+    var firebaseInput = document.getElementById("firebase-input");
 
+    messageSpace.innerHTML = "You've connected to the JavaScript!";
+
+    document.getElementById("btn").onclick = () => {
+        var msg;
+        if (input.value == undefined || input.value == null || input.value == ""){
+            msg = "Please enter text in input, then try again"
+        } else {
+            msg = input.value;
+        }
+
+        document.getElementById("heading").innerHTML = msg;
+    }
+
+    document.getElementById("firebase-btn").onclick = () => {
+        if (firebaseInput.value == undefined || firebaseInput.value == null || firebaseInput.value == ""){
+            alert("Please type into the input");
+        } else {
+            fb_write(`messages/${Date.now()}`, firebaseInput.value, (_data, _path) => {
+                alert(`Sucessfully Wrote ${_data} @ ${_path}`);
+            })
+        }
+    }
+
+    async function fb_write(_path, _data, _callback = null) {
+        try {
+            await set(ref(getDatabase(app), _path), _data)
+            if (_callback != null && _callback instanceof Function) _callback(_data, _path);
+        } catch (_error){
+            alert(`Error Writing ${_data} @ ${_path}: ${_error}`);
+        }
+    }
 
